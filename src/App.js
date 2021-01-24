663ef22a22bba586fb24e90def8fdd81120ad5ff
廣本　祥 <shoprogramsa@gmail.com>
2021-01-24 20:01:41 +0900

    最初のコミット

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React on SHO
         </a>
       </header>
     </div>
